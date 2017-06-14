5776132ab26ae0f00a9598a063bf30e63ef41ada
Jens Stimpfle <jfs@jstimpfle.de>
2017-06-14 07:27:15 +0200

    update build-prebuilt.sh to new manpage directory

--- rb3ptr/build-prebuilt.sh
+++ rb3ptr/build-prebuilt.sh
@@ -1,3 +1,2 @@
 rsync -a --delete build/ prebuilt/
-man -Tutf8 ./rb3ptr.3 | col -b > prebuilt/rb3ptr.3.txt
-man -Tps ./rb3ptr.3 | col -b | ps2pdf - - > prebuilt/rb3ptr.3.pdf
+man -Tps man3/rb3ptr.3 | col -b | ps2pdf - - > prebuilt/rb3ptr.3.pdf
